318b718ed1827f838bcd470bd5e86e7559916723
Katop <kathan@berkeley.edu>
2022-08-30 15:24:26 -0700

    set up interpreter and retrieved video with cv2

---
+++ helpers.py
@@ -0,0 +1,6 @@
+
+def get_video_filename():
+    # todo you may have to change this for the deliverable
+    return "/home/katop/Desktop/1678_3566_final_four.webm.mp4"
+
+
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 # model to detect all person frames
+import cv2, os
+from helpers import *
+
+video_filename = get_video_filename()
+cap = cv2.VideoCapture(video_filename)
+
+while True:
+   ret, frame = cap.read()
+   cv2.imshow('frame',frame)
+   if cv2.waitKey(1) & 0xFF == ord('q') or ret==False:
+       cap.release()
+       cv2.destroyAllWindows()
+       break
+   cv2.imshow('frame',frame)
 
 # have a helper function that can pull up the person object's frame for a target
 # frame to make manual classification easier.
